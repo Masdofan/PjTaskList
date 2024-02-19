c6c6ed43a31b7f4381de1f09cde2d5ae46442d16
Ilya Mishin <Masdofan@yandex.ru>
2024-02-19 23:41:19 +0300

    Initial commit

---
+++ src/main/java/com/example/pjtasklist/PjTaskListApplication.java
@@ -0,0 +1,13 @@
+package com.example.pjtasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjTaskListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjTaskListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pjtasklist/PjTaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pjtasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjTaskListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
